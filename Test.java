52e77c76893b3cf2df7e95aa5209a500a680270d
Test_developer <1041622191@qq.com>
2024-01-24 16:33:25 +0800

    test

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test {
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("aa"+j + "*" + i + "=" + i * j + '\t');//在这里使用了'\t'
+                System.out.print("a"+j + "*" + i + "=" + i * j + '\t');//在这里使用了'\t'
             }
             System.out.println();//在内循环外面加上换行
         }
