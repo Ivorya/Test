16755e91d22614c1e9dc4d38561a096dd7a24046
Test_developer <1041622191@qq.com>
2024-01-25 15:15:19 +0800

    test

---
+++ Read.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Read {
+    int readNum;
+
+
+    public Read(int ReadN){
+        this.readNum = ReadN;
+
+    }
+
+    public int getReadNum(){
+        return this.readNum;
+    }
+
+
+}
